31de85f6dfa0893ba4152efc4dc46d734c246f84
HungLe <hunglp2k1@gmail.com>
2023-04-12 00:15:46 +0700

    UI and Code Home

---
+++ fast_food_app/Controllers/MenuLoaiController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using fast_food_app.Models;
+using System.Collections;
+
+namespace fast_food_app.Controllers
+{
+    public class MenuLoaiController : Controller
+    {
+        // GET: MenuLoai
+        public ActionResult Index()
+        {
+            using (FastFoodEntities db = new FastFoodEntities())
+            {
+                var loaimonan = db.LOAIMONANs.ToList();
+                Hashtable tenloai = new Hashtable();
+                foreach (var item in loaimonan)
+                {
+                    tenloai.Add(item.MaLoaiMonAn, item.TenLoaiMonAN);
+                }
+                ViewBag.TenLoai = tenloai;
+                return PartialView("Index");
+            }
+        }
+    }
+}
---
+++ fast_food_app/Models/Thongke.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace fast_food_app.Models
+{
+    public class Thongke
+    {
+    }
+}
