47d740c0a2191c9e919b9d83dec19530b6bef787
An <leluutruongan@gmail.com>
2023-05-30 16:43:14 +0700

    Login admin

---
+++ fast_food_app/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace fast_food_app.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            if (Session["username"] == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
